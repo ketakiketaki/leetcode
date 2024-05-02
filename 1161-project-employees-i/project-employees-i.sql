39edfc7d7e014dc5239fa44d82585b8b4d54aa53
ketakiketaki <56091065+ketakiketaki@users.noreply.github.com>
2024-05-02 17:44:41 +0530

    Time: 1833 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1161-project-employees-i/project-employees-i.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT p.project_id, IFNULL(ROUND(AVG(e.experience_years),2),1) AS average_years
+FROM Project p
+LEFT JOIN Employee e
+ON p.employee_id = e.employee_id
+GROUP BY p.project_id
